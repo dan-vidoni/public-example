966c57d81775e06baec04237b221bca56e211ad8
dan vidoni <dan.vidoni@haufe.com>
2026-04-26 20:15:00 +0200

    add test

--- src/test/java/de/nufin/interviews/ApplicationTests.java
+++ src/test/java/de/nufin/interviews/ApplicationTests.java
@@ -1,13 +1,22 @@
 package de.nufin.interviews;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class ApplicationTests {
 
+    @Autowired
+    private ApplicationContext applicationContext;
+
+
     @Test
     void contextLoads() {
+        System.out.println("In test");
+        assertThat(applicationContext).isNotNull();
     }
-
 }
